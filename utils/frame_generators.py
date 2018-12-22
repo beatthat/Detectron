a421f69ac58d197ade6aa5b47c3fa85704ec197d
larrykirschner <larrykirschner@gmail.com>
2018-12-22 12:32:16 -0800

    video2frames generator takes params frame_count, frame_stride, frame_start

--- utils/frame_generators.py
+++ utils/frame_generators.py
@@ -1,22 +1,61 @@
 import cv2
 import time
 
-def video2frames(video_path, logging=False):
+def video2frames(
+    video_path, 
+    frame_start = 0,
+    frame_count = None,
+    frame_stride = 1,
+    logging=False):
+    """Generator yields image frames given a video path.
+
+    `PEP 484`_ type annotations are supported. If attribute, parameter, and
+    return types are annotated according to `PEP 484`_, they do not need to be
+    included in the docstring:
+
+    Args:
+        video_path (pathlike): path to a video file
+
+        frame_start: frame number to yield first. Defaults to 0.
+
+        frame_count: number of frames to yield total. Defaults None (end of video)
+
+        frame_stride: yield every Nth frame. Defaults to 1.
+
+        logging (bool, optional): enable verbose console logging. Defaults to False.
+
+    Returns:
+        yields image frames
+
+    """
     cap = cv2.VideoCapture(video_path)
-    i = 0
+    f = 0
     while(cap.isOpened()):
-        t = time.time()
+        yf = f - frame_start
+
+        f += 1
         
         ret, frame = cap.read()
+
+
+        if yf < 0 or yf % frame_stride != 0:
+            continue
+
+        yf /= frame_stride
+
+        if frame_count is not None and yf >= frame_count:
+            break
+
+        t = time.time()
+        
         
         if not ret:
             break
             
         if logging:
-            print('read frame ' + str(i) + ' in ' + str(time.time() - t) + "s")
+            print('read frame {} and yield as {} in {}s'.format(str(f), str(yf), str(time.time() - t)))
         
         yield frame
-        i += 1
         
     cap.release()
 
